35b9e9d57e9e07964b51e72c993305e19751a726
Your Name <you@example.com>
2025-03-10 22:11:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+function findMedian(arr) {
+    const sortedArr = [...arr].sort((a, b) => a - b);
+    const mid = Math.floor(sortedArr.length / 2);
+
+    return sortedArr.length % 2 !== 0 ? sortedArr[mid] : (sortedArr[mid - 1] + sortedArr[mid]) / 2;
+}
+
+console.log(findMedian([10, 2, 14, 4, 7, 6]));
+
+function findMostDigits(arr, n) {
+return [...arr].sort((a, b) => a.toString -b.toString).slice(0, n);
+}
+
+console.log(findMostDigits([1000, 200, 14, 4, 7, 6], 2));
+
+function findDivisibleByX(arr, x) {
+ return arr.filter((num) => num % x === 0)
+}
+
+console.log(findDivisibleByX([1000, 200, 14, 4, 7, 6], 2));
+
+
+//! 1 Find the first n prime numbers in an array.
+function findthreeLargestNumbers(arr) {
+    return arr.sort((a, b) => a - b).slice(0,3);
+}
+
+console.log(findthreeLargestNumbers([1000, 200, 14, 400, 7, 6]));
+
+//! 2 Find the first n prime numbers in an array.
+function findthreedabeuletNumbers(arr) {
+    return arr
+    .sort((a, b) => a - b)
+    .filter((num) => num % 3 === 0)
+    .slice(0,3);
+}
+
+console.log(findthreedabeuletNumbers([1000, 600, 16, 900, 9, 6]));
+
+
+
+//! 3  Find numbers in an array that end with 5.
+function findNumbersEndWith5(arr) {
+    return arr
+    .filter((num) => num
+    .toString()
+    .endsWith('5'));
+}
+
+console.log(findNumbersEndWith5([1005, 605, 16, 900, 9, 6]));
+
+//! 4. Find all perfect squares in an array.
+
+function findPerfectSquares(arr) {
+    return arr
+        .filter(num => Math.sqrt(num) % 1 === 0)
+        .map(num => ({ number: num, squareRoot: Math.sqrt(num) }));
+}
+
+console.log(findPerfectSquares([100, 605, 16, 900, 9, 6]));
+
+//! 5. Find numbers in an array whose sum of digits is even.
+function findNumbersWithEvenSumOfDigits(arr) {
+    return arr
+        .filter(num => [...num.toString()]
+            .reduce((acc, curr) => acc + parseInt(curr), 0) % 2 === 0);
+}   
+
+console.log(findNumbersWithEvenSumOfDigits([100, 605, 16, 900, 9, 6]));
+
+//! 6. Find all numbers that contain the digit 7.
+
+function findNumberscon5(arr) {
+    return arr
+    .filter((num) => num
+    .toString()
+    .includes('7'));
+}
+
+console.log(findNumberscon5([17805, 605, 17, 900, 9, 6]));
+
+//! 7. Find numbers that are greater than the average of the array.
+
+function findNumbersGreaterThanAverage(arr) {
+    const average = arr.reduce((   acc, curr) => acc + curr, 0) / arr.length;
+    return arr.filter(num => num>average)
+}
+console.log(findNumbersGreaterThanAverage([100, 605, 16, 900, 9, 6]));
+
+//! 8. Find the first n numbers that are NOT divisible by 2 or 3.
+
+function findNumbersNotDivisibleBy2Or3(arr, n) {
+    return arr
+        .sort((a, b) => a - b) 
+        .filter(num => num % 2 !== 0 && num % 3 !== 0) 
+        // .slice(0, n);
+}
+
+console.log(findNumbersNotDivisibleBy2Or3([100, 605, 16, 900, 9, 2, 5, 6], 3));
+
+//! 9.Find all numbers that are **palindromes**.
+
+function findPalindromes(arr) {
+    return arr.filter(num => {
+        let strNum = num.toString();
+        return strNum === strNum.split('').reverse().join('');
+    });
+}
+
+console.log(findPalindromes([121, 234, 909, 456, 787, 1001, 99, 12]));
